90abb31aaad7c7a5d8becec539dc921e910ea0b4
Matt Wang <ymattw@users.noreply.github.com>
2022-05-20 16:23:42 +0200

    Add note of MacVim pref setting

--- editserver.sh
+++ editserver.sh
@@ -5,5 +5,6 @@
 # python3 -m pip install chrome-edit-server
 # python3 -m pip show chrome-edit-server
 
+# Configure MacVim to quit after last window is closed: MacVim -> Preferences -> General
 export EDIT_SERVER_EDITOR="open -nWa MacVim"
 $HOME/Library/Python/3.8/bin/chrome-edit-server
