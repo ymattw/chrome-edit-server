4e3348126ed86fad3576010b3513fa37c9510b48
Matt Wang <ymattw@users.noreply.github.com>
2025-01-23 15:37:06 +0100

    Update example editserver.sh wrapper script

--- editserver.sh
+++ editserver.sh
@@ -2,9 +2,14 @@
 #
 # Example wrapper script for macOS
 #
-# python3 -m pip install chrome-edit-server
-# python3 -m pip show chrome-edit-server
+# pip install .
+# pip show chrome-edit-server
 
-# Configure MacVim to quit after last window is closed: MacVim -> Preferences -> General
-export EDIT_SERVER_EDITOR="open -nWa MacVim"
-$HOME/Library/Python/3.8/bin/chrome-edit-server
+# If use MacVIM: configure MacVim to quit after last window is closed: MacVim
+# -> Preferences -> General
+# export EDIT_SERVER_EDITOR="open -nWa MacVim"
+
+# Use Neovide
+export EDIT_SERVER_EDITOR="open -nWa neovide"
+
+nohup chrome-edit-server >& /tmp/editserver.log &
